6289c93d09711a197cefeac79b41cf18f144ce63
jaweed3 <wedjaw22@gmail.com>
2025-03-22 17:02:07 +0700

    NotfoundPage.js

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Typography, Button, Container } from '@mui/material';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import HomeIcon from '@mui/icons-material/Home';
+
+const NotFoundPage = () => {
+  return (
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '60vh',
+          textAlign: 'center',
+        }}
+      >
+        <ErrorOutlineIcon sx={{ fontSize: 100, color: 'text.secondary', mb: 2 }} />
+        <Typography variant="h2" component="h1" gutterBottom>
+          404
+        </Typography>
+        <Typography variant="h4" gutterBottom>
+          Page Not Found
+        </Typography>
+        <Typography variant="body1" color="text.secondary" paragraph>
+          The page you are looking for doesn't exist or has been moved.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to="/"
+          startIcon={<HomeIcon />}
+          sx={{ mt: 2 }}
+        >
+          Back to Home
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
